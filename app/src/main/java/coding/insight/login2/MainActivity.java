74734cc2226064876bd7c75016166b2ff7c9da42
poplolpush <poplolpush@gmail.com>
2020-12-04 21:46:32 +0530

    Initial Commit...

---
+++ app/src/main/java/coding/insight/login2/MainActivity.java
@@ -0,0 +1,14 @@
+package coding.insight.login2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
